ff1e4e65bdce692cf70d5c5ca758965761c76c0d
caspermike997 <caspermike97@gmail.com>
2023-09-14 07:55:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import HomePage from "./pages/HomePage"
+import MovieDetails from './pages/MovieDetails'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="movies/:id" element={<MovieDetails />} /> */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+        <div className="footer-icons">
+        <span>{icons.facebook}</span>
+        <span>{icons.instagram}</span>
+        <span>{icons.twitter}</span>
+        <span>{icons.youtube}</span>
+        </div>
+
+        <div className="fotter-content">
+            <h3 className="Footer-text">Conditions of Use</h3>
+            <h3>Privacy & Policy</h3>
+            <h3>Press Room</h3>
+        </div>
+
+        <div>
+            <h2>  © 2021 MovieBox by Adriana Eka Prayudha  </h2>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
